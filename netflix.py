0682bb85f80bdff8ab6213c668489fce478dd224
rosindubh <p.welsby2@ntlworld.com>
2017-02-09 23:36:44 +0000

    remove excess code which was not being used.

--- netflix.py
+++ netflix.py
@@ -8,7 +8,3 @@ path = '/home/phil/my_python_programs/netflix_codes'
 
 code =input('enter code: ')
 webbrowser.open('http://www.netflix.com/browse/genre/' + code)
-
-#with open(path) as codes_file:
-#    for line in codes_file:
-#        print(line)
