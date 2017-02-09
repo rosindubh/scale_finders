0c8130ced78f01f968f479a81e5b7aa2f041e5c8
rosindubh <p.welsby2@ntlworld.com>
2017-02-09 23:23:40 +0000

    set web page on sets.py - learn_python.py is a test program

--- sets.py
+++ sets.py
@@ -1,4 +1,5 @@
 # this shows an example set and a link to a youtube video about them
 # phil welsby - 22 january 2017
-example = set()
-webbrowser.open(https://youtu.be/sBvaPopWOmQ?list=PLi01XoE8jYohWFPpC17Z-wWhPOSuh8Er-)
+import webbrowser
+#example = set()
+webbrowser.open('https://www.youtube.com/watch?v=sBvaPopWOmQ')
