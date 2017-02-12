5885afb5cfc98b18cf9f0ef9814881a169806b07
rosindubh <p.welsby2@ntlworld.com>
2017-02-12 16:02:54 +0000

    started to add video variables and a main() part to choose a vid

--- take_a_break.py
+++ take_a_break.py
@@ -1,2 +1,34 @@
+# program to choose a youtube video at random
+# phil welsby - 12 february 2017
+
 import webbrowser
-webbrowser.open('https://youtube.com/watch?v=qEaf9LqIUZQ')
+import random
+
+# list of videos
+opeth_01 = 'https://youtube.com/watch?v=qEaf9LqIUZQ'  # windowpane
+opeth_02 = 'https://www.youtube.com/watch?v=MDBykpSXsSE'  # ghost of predition
+foo_fighters_01 = 'https://www.youtube.com/watch?v=SBjQ9tuuTJQ'  # the pretender
+nickleback_01 = 'https://www.youtube.com/watch?v=T3rXdeOvhNE'  # photograph
+
+# print menu to screen
+print('''(1)	opeth		windowpane
+(2)	opeth		ghost of predition
+(3)	foofighters	the pretender	
+(4)	nickleback	photograph
+''')
+
+# generate a random choice
+#choice = random.randint(1, 10)
+choice = int(input('Enter your choice'))
+
+# main
+if choice == 1:
+    webbrowser.open(opeth_01)
+elif choice == 2:
+    webbrowser.open(opeth_02)
+elif choice == 3:
+    webbrowser.open(foo_fighters_01)
+elif choice == 4:
+    webbrowser.open(nickleback_01)
+else:
+    print('need more videos welsby, pull ya finger out!')
