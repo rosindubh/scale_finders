a9d2040ab56f5afefa91fba839d4ec4474e86540
rosindubh <p.welsby2@ntlworld.com>
2017-02-15 00:03:25 +0000

    added radio stations

--- take_a_break.py
+++ take_a_break.py
@@ -15,18 +15,32 @@ opeth_01 = 'https://youtube.com/watch?v=qEaf9LqIUZQ'  # windowpane
 opeth_02 = 'https://www.youtube.com/watch?v=MDBykpSXsSE'  # ghost of predition
 foo_fighters_01 = 'https://www.youtube.com/watch?v=SBjQ9tuuTJQ'  # the pretender
 nickleback_01 = 'https://www.youtube.com/watch?v=T3rXdeOvhNE'  # photograph
-gorillaz_01 = 'https://www.youtube.com/watch?v=HyHNuVaZJ-k'
-
+gorillaz_01 = 'https://www.youtube.com/watch?v=HyHNuVaZJ-k'  # feel good inc
+my_chemical_romance_01 = 'https://www.youtube.com/watch?v=RRKJiM9Njr8'
+greenday_01 = 'https://www.youtube.com/watch?v=NUTGr5t3MoY'
+nirvana_01 = 'https://www.youtube.com/watch?v=hTWKbfoikeg'
+xs_radio = 'http://www.xsmanchester.co.uk/radio/player/'
+radio_manchester = 'http://www.bbc.co.uk/radio/player/bbc_radio_manchester'
 # print menu to screen
 print('''
 		  ***YouTube Videos***
 
 
-	(1)	opeth		windowpane
-	(2)	opeth		ghost of predition
-	(3)	foofighters	the pretender	
-	(4)	nickleback	photograph
-	(5)	gorillaz	feel good inc
+	(1)	opeth			windowpane
+	(2)	opeth			ghost of predition
+	(3)	foofighters		the pretender	
+	(4)	nickleback		photograph
+	(5)	gorillaz		feel good inc
+	(6)	my chemical romance	welcome to the black parade
+	(7)	greenday		basket case
+	(8)	nirvana			smells like teen spirit
+
+		      ***Local Radio***
+
+	(9)	xs radio		local radio
+	(10)	radio manchester	local radio
+
+
 ''')
 
 # generate a random choice
@@ -46,5 +60,16 @@ elif choice == 4:
     webbrowser.open(nickleback_01)
 elif choice == 5:
     webbrowser.open(gorillaz_01)
+elif choice == 6:
+    webbrowser.open(my_chemical_romance_01)
+elif choice == 7:
+    webbrowser.open(greenday_01)
+elif choice == 8:
+     webbrowser.open(nirvana_01)
+elif choice == 9:
+    webbrowser.open(xs_radio)
+elif choice == 10:
+    webbrowser.open(radio_manchester)
+
 else:
     print('need more videos welsby, pull ya finger out!')
